bde94eb9be81c96b1a69b6fb808a5eaf6a988bc9
Elise Jones <02elise.jones02@gmail.com>
2023-06-07 17:40:11 -0600

    Create get request for data

---
+++ src/apiCalls.js
@@ -0,0 +1,13 @@
+const fetchAPI = (dataType) => {
+  return fetch(`http://localhost:3001/api/v1/${dataType}`)
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => errorHandling(err));
+};
+
+const errorHandling = (err) => {
+  alert(`${err.name}: ${err.message}!\nWhat's Cookin failed to obtain data from the server.`);
+};
+
+export { fetchAPI }
