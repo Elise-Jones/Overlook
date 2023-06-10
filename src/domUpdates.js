00cfe093d9feb05cda613f4220256d8fe1bf0029
Elise Jones <02elise.jones02@gmail.com>
2023-06-10 11:31:41 -0600

    Add function for displaying the bookings for customer on window load

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,15 @@
-import { userTotal } from './scripts.js'
+import { userTotal, bookingContainer } from './scripts.js'
 const renderTotalPrice = (currentCustomer) => {
   userTotal.innerText = currentCustomer.totalPrice
 }
-export { renderTotalPrice}
+
+const renderBookedRooms = (customerBookings) => {
+  customerBookings.forEach((room) => {
+    bookingContainer.innerHTML += `<article class="bookingcard" id="${room.id}">
+   Date Booked: ${room.dateBooked} <br> Room Type: ${room.roomType} <br> room number : ${room.roomNumber} <br> Number of Beds: ${room.numBeds} <br> Cost Per Night: ${room.costPerNight}
+    </article>`
+  })
+}
+
+console.log('vava', 'lkjasdflkjsa')
+export { renderTotalPrice, renderBookedRooms}
