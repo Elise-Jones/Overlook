9f1413d7be1567a74753eb04c59e5184889d62d5
Elise Jones <02elise.jones02@gmail.com>
2023-06-11 13:34:22 -0600

    Successfully filter when type selected.

--- src/scripts.js
+++ src/scripts.js
@@ -10,7 +10,7 @@ import { getRandomCustomer } from './customer.js';
 import { fetchAPI } from './apiCalls'
 import { calculatePrice } from './calculate-price';
 import { renderTotalPrice, renderBookedRooms, showDomElement, hideDomElement } from './domUpdates';
-import { filterAlreadyBookedRooms, filterAvailableRoomsByDate, createDisplayingObjectForDate } from './filter-functions'
+import { filterAlreadyBookedRooms, createDisplayingObjectForDate, filterByType } from './filter-functions'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
@@ -19,7 +19,7 @@ let currentCustomer;
 let bookingdata
 let customerData;
 let roomData;
-
+let dateMatchedArray
 
 
 
@@ -31,23 +31,21 @@ const calendarSubmitButton = document.querySelector('.calendarsubmitbutton')
 const startDate = document.querySelector(".startdate")
 const byDateContainer = document.querySelector(".bydate")
 const datePicker = document.getElementById("datepicker")
+const roomTypeButtonHolder = document.querySelector(".buttonholder")
 
 
 const start = () => {
   Promise.all([fetchAPI('customers'), fetchAPI('bookings'), fetchAPI('rooms')]).then((data) => {
     customerData = data[0].customers;
-    console.log('customerData', customerData)
     bookingdata = data[1].bookings
     roomData = data[2].rooms
-    console.log('roomdaata', roomData)
     currentCustomer = getRandomCustomer(customerData);
     currentCustomer.totalPrice = calculatePrice(currentCustomer, bookingdata, roomData)
     const customerBookings = filterAlreadyBookedRooms(currentCustomer, bookingdata, roomData)
-    console.log(customerBookings.length)
     renderTotalPrice(currentCustomer) 
     renderBookedRooms(customerBookings, bookingContainer)
     datePicker.min= new Date().toISOString().split("T")[0];
-    console.log(datePicker)
+
   });
  
 };
@@ -56,9 +54,16 @@ const start = () => {
 window.addEventListener('load', start);
 calendarSubmitButton.addEventListener('click', (e) => {
   e.preventDefault()
-  const cre = createDisplayingObjectForDate(startDate.value, roomData, bookingdata)
+  dateMatchedArray = createDisplayingObjectForDate(startDate.value, roomData, bookingdata)
   hideDomElement(bookingContainer)
   showDomElement(byDateContainer)
-  renderBookedRooms(cre, byDateContainer)
+  renderBookedRooms(dateMatchedArray, byDateContainer)
+  })
+
+  roomTypeButtonHolder.addEventListener('click', (e)=>{
+    console.log(dateMatchedArray)
+  console.log(e.target.value)
+   let type = filterByType(dateMatchedArray, e.target.value)
+   console.log(type)
   })
 export { userTotal, bookingContainer }
